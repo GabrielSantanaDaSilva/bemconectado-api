a8ece0dc53d5444b12ecae374b778496fad7e67c
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-07-05 13:29:44 -0300

    Model da tabela de categorias

--- bemconectado/src/main/java/com/generation/bemconectado/model/Categoria.java
+++ bemconectado/src/main/java/com/generation/bemconectado/model/Categoria.java
@@ -1,5 +1,24 @@
 package com.generation.bemconectado.model;
 
-public class Categoria {
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	
+	
+	
 }

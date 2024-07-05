e51944d3127891468a208132d6b50eedbbac185a
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-07-05 11:42:28 -0300

    criação do projeto Maven e configuração do application.properties

---
+++ bemconectado/src/main/java/com/generation/bemconectado/BemconectadoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.bemconectado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemconectadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemconectadoApplication.class, args);
+	}
+
+}
